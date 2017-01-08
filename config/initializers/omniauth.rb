d1c2d77bb2a0d13d2c076919e0ae2f800b97e791
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-01-08 20:14:11 +0100

    set twitter to read-only

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],
+           x_auth_access_type: 'read'
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
 
