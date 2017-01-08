a2a8f4365f492701f1ee648c7ac5254d24ed0e82
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-01-08 21:17:34 +0100

    simplified user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,4 @@ class ApplicationController < ActionController::Base
     end
   end
   helper_method :current_user
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
-
   def self.find_or_create_with_omniauth(auth)
-    # look for an existing authorisation 
+    # look for an existing authorisation
     # provider + uid uniquely identify a user
-    user = User.find_or_create_by( 
-           provider: auth['provider'], 
-           uid:      auth['uid'] 
+    User.find_or_create_by!(
+      provider: auth['provider'],
+      uid:      auth['uid']
     )
-    user.save!
-    return u
-  end  
+  end
 end
