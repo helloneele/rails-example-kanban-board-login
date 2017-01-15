df384f76eeef99cae5c213fae563302902a71f6b
helloneele <hello@neele.codes>
2017-01-15 10:26:51 +0100

    added github auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     resources :cards
   end
   root 'boards#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
 
+  get '/logout', to: 'sessions#destroy'
   match '/auth/:provider/callback', to: 'sessions#create',  via: [:get, :post]
   match '/auth/failure',            to: 'sessions#failure', via: [:get, :post]
 end
